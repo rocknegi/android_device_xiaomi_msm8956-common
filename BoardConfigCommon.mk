7539c2ba7b07ad77a272db30aed45ac6e88ff95d
rocknegi <rocknegi53@gmail.com>
2017-12-31 07:26:29 +0000

    remove dexpreopt

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,18 +112,6 @@ WITH_CUSTOM_CHARGER := false
 # CNE
 BOARD_USES_QCNE := true
 
-# Dexpreopt
-ifeq ($(CARDINAL_RELEASE),true)
-ifeq ($(HOST_OS),linux)
-ifneq ($(TARGET_BUILD_VARIANT),eng)
-WITH_DEXPREOPT := true
-WITH_DEXPREOPT_DEBUG_INFO := false
-USE_DEX2OAT_DEBUG := false
-DONT_DEXPREOPT_PREBUILTS := true
-endif
-endif
-endif
-
 # Display
 MAX_VIRTUAL_DISPLAY_DIMENSION := 2048
 TARGET_CONTINUOUS_SPLASH_ENABLED := true
