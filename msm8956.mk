c9c592d44a28fd7cf481ee059ed1d033449f7fcd
rocknegi <rocknegi53@gmail.com>
2018-01-26 15:27:24 +0530

    add live wallpaper support

--- msm8956.mk
+++ msm8956.mk
@@ -293,6 +293,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
++# Live Wallpapers
+PRODUCT_PACKAGES += \
+    LiveWallpapersPicker \
+    librs_jni    
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
